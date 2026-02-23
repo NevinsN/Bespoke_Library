e521e555220e887cf15ca47fe4ac3cc7d1752c61
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2026-02-22 17:41:21 -0700

    Update function_app.py

--- api/function_app.py
+++ api/function_app.py
@@ -1,13 +1,11 @@
 import azure.functions as func
-# Import your existing handlers
+# Import the modular handlers
 from routes.novel_routes import handle_get_novels, handle_get_chapters, handle_get_content
-# Import the new author studio handlers (You'll create these next)
-from routes.author_routes import handle_upload_draft, handle_update_meta
+from routes.author_routes import handle_upload_draft, handle_update_meta, handle_create_project
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # --- READER / GENERAL ROUTES ---
-
 @app.route(route="GetNovels", methods=["GET"])
 def get_novels(req: func.HttpRequest) -> func.HttpResponse:
     return handle_get_novels(req)
@@ -20,9 +18,7 @@ def get_chapters(req: func.HttpRequest) -> func.HttpResponse:
 def get_chapter_content(req: func.HttpRequest) -> func.HttpResponse:
     return handle_get_content(req)
 
-
 # --- AUTHOR STUDIO ROUTES ---
-
 @app.route(route="UploadDraft", methods=["POST"])
 def upload_draft(req: func.HttpRequest) -> func.HttpResponse:
     return handle_upload_draft(req)
@@ -30,3 +26,7 @@ def upload_draft(req: func.HttpRequest) -> func.HttpResponse:
 @app.route(route="UpdateNovelMeta", methods=["POST"])
 def update_meta(req: func.HttpRequest) -> func.HttpResponse:
     return handle_update_meta(req)
+
+@app.route(route="CreateProject", methods=["POST"])
+def create_project(req: func.HttpRequest) -> func.HttpResponse:
+    return handle_create_project(req)
