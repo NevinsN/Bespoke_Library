f2e08963916db4a912299dfbc94887833ebad63a
Nicholas Nevins <47232970+NevinsN@users.noreply.github.com>
2026-02-23 09:55:10 -0700

    Update response.py

--- api/utils/response.py
+++ api/utils/response.py
@@ -1,18 +1,22 @@
-# utils/response.py
-
 import json
 import azure.functions as func
 
-def ok(data):
+def ok(data, meta=None):
+    payload = {
+        "success": True,
+        "data": data
+    }
+    if meta:
+        payload["meta"] = meta
     return func.HttpResponse(
-        json.dumps({"success": True, "data": data}),
+        body=json.dumps(payload),
         status_code=200,
         mimetype="application/json"
     )
 
 def error(message, code=500):
     return func.HttpResponse(
-        json.dumps({"success": False, "error": message}),
+        body=json.dumps({"success": False, "error": message}),
         status_code=code,
         mimetype="application/json"
     )
